8bb3054bd024961a1d69daba2e06709631c5d09b
Monika39-maker <dangolmonika@yahoo.com>
2022-04-29 14:01:45 +0100

    new changes

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -1,14 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
-import laurarose from 'C:/Users/vicky/OneDrive/Desktop/New folder/my-protfolio/src/lauraimg-edited.png';
-import kamaneimg from 'C:/Users/vicky/OneDrive/Desktop/New folder/my-protfolio/src/kamaneimg-edited.png';
-import sabinaimg from 'C:/Users/vicky/OneDrive/Desktop/New folder/my-protfolio/src/sabinaimg-edited.png';
-import { Parallax, Background } from "react-parallax";
-=======
 import laurarose from '../../lauraimg-edited.png';
 import kamaneimg from '../../kamaneimg-edited.png';
 import sabinaimg from '../../sabinaimg-edited.png';
->>>>>>> e2524fac04e05d18e9f594a8f4d4787f6605a3d7
 
 
 function Projects() {
@@ -30,11 +23,7 @@ function Projects() {
                         <h3 className="text-center">Django based Ecommerce Website</h3>
                         <p>This website uses Python and Django. It saves registered users information in postgresql database. All the images are saved in AWS. It also uses bcrypt to safeguard the users data. At the end of checkout process, the total of price is calculated and retrieved from the backend instead of frontend so that it cannot be manupulated via javascript. The paypal api is used to enabe the transaction between the user and the client</p>
                     </div>
-<<<<<<< HEAD
-                    
-=======
                     <a href="https://vast-reaches-96167.herokuapp.com/"><img src={sabinaimg} className="project-img" alt="sabina-web"/></a>
->>>>>>> e2524fac04e05d18e9f594a8f4d4787f6605a3d7
                 </div>
                 <div className="proj">
                     <a href="https://kamane.herokuapp.com/"><img src={kamaneimg} className="project-img" alt='kamaneWeb'/></a>
