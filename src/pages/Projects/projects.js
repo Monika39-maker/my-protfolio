52b0dd90cfda8ebafe36a36bb78db0f5a7d34d03
Monika39-maker <dangolmonika@yahoo.com>
2022-04-30 15:46:36 +0100

    contact file edited

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -23,7 +23,7 @@ function Projects() {
                         <h3 className="text-center">Django based Ecommerce Website</h3>
                         <p>This website uses Python and Django. It saves registered users information in postgresql database. All the images are saved in AWS. It also uses bcrypt to safeguard the users data. At the end of checkout process, the total of price is calculated and retrieved from the backend instead of frontend so that it cannot be manupulated via javascript. The paypal api is used to enabe the transaction between the user and the client</p>
                     </div>
-                    <a href="https://vast-reaches-96167.herokuapp.com/"><img src={sabinaimg} className="project-img" alt="sabina-web"/></a>
+                    
                 </div>
                 <div className="proj">
                     <a href="https://kamane.herokuapp.com/"><img src={kamaneimg} className="project-img" alt='kamaneWeb'/></a>
--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {AiOutlineMail} from 'react-icons/ai';
 import {AiOutlinePhone} from 'react-icons/ai';
-import {AiOutlineMessage} from 'react-icons/ai'
+import {AiOutlineMessage} from 'react-icons/ai';
+import $ from 'jquery'
 
-export default function Contact() {
-    return (
+export default class Contact extends Component {
+    jQuerycode = () => {
+        $('.contact-1').show()
+         $('.contact-2').hide()
+           $('.contact-3').hide()
+           $('.contact-4').hide()
+        $('.icon1').on("click", function() {
+            $('.contact-1').show()
+            $('.contact-2').hide()
+           $('.contact-3').hide()
+           $('.contact-4').hide()
+        });
+        $('.icon2').on("click", function() {
+            $('.contact-1').hide()
+            $('.contact-2').show()
+           $('.contact-3').hide()
+           $('.contact-3').hide()
+        });
+        $('.icon3').on("click", function() {
+            $('.contact-1').hide()
+            $('.contact-2').hide()
+           $('.contact-3').show()
+           $('.contact-4').hide()
+        });
+        $('.icon4').on("click", function() {
+            $('.contact-1').hide()
+            $('.contact-2').hide()
+           $('.contact-3').hide()
+           $('.contact-4').show()
+        });
+    }
+
+    componentDidMount() {
+        this.jQuerycode()
+    }
+
+
+    render() {
+        return (
         <div>
             <div className="text-center">
                 <h2>Contact</h2>
@@ -25,5 +63,6 @@ export default function Contact() {
             
         </div>
     )
+    }
 }
 
